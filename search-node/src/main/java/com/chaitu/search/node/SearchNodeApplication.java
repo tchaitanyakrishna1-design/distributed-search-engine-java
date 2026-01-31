f825d2aa72e01d0c51f84babde88d4bfa87b2789
tchaitanyakrishna1-design <tchaitanyakrishna1@gmail.com>
2026-02-01 00:18:14 +0530

    Initialize search-node Spring Boot service

---
+++ search-node/src/main/java/com/chaitu/search/node/SearchNodeApplication.java
@@ -0,0 +1,13 @@
+package com.chaitu.search.node;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchNodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchNodeApplication.class, args);
+	}
+
+}
---
+++ search-node/src/test/java/com/chaitu/search/node/SearchNodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaitu.search.node;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchNodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
