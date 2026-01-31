35c6c4d55935e7ae2f84d882dfbe053790fc0d92
tchaitanyakrishna1-design <tchaitanyakrishna1@gmail.com>
2026-01-31 23:29:00 +0530

    Initialize coordinator service with Spring Boot

---
+++ coordinator-service/src/test/java/com/chaitu/search/coordinator/CoordinatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaitu.search.coordinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoordinatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
