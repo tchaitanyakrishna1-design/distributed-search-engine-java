95a3f00a08d0ce310eebf45646f7f81b2b60a804
tchaitanyakrishna1-design <tchaitanyakrishna1@gmail.com>
2026-02-01 00:01:43 +0530

    Add health check endpoint to coordinator service

---
+++ coordinator-service/HealthController.java
@@ -0,0 +1,13 @@
+package com.chaitu.search.coordinator;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "Coordinator service is running";
+    }
+}
